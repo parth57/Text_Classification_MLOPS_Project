8947e2dde04a6289eb78ba31e05022ddacf57ae1
Parth Agarwal <parthagarwal57@gmail.com>
2026-01-01 14:58:44 +0530

    Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It's an E2E MLOPS Project on Text Classification',
+    author='Parth Agarwal',
+    license='MIT',
+)
